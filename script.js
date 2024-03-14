8f734cee1076a14752af7300325d1a580e26d100
JulceLucas <julcelucaslucas@gmal.com>
2024-03-14 23:08:38 +0100

    novo

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ function nextImage(){
     document.getElementById("radio"+count).checked = true;
 
 }
+
+/*Script De Noticias*/
+
+
+
+    function scrollr() {
+        var left = document.querySelector(".scroll");
+        left.scrollBy(350, 0)
+    }
+    function scrolll() {
+        var right = document.querySelector(".scroll");
+        right.scrollBy(-350, 0)
+    }
+
+
+
+
+
+
+    
+        // Função para verificar a rolagem horizontal
+        function handleHorizontalScroll() {
+            // Obtenha a posição da rolagem horizontal
+            var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+            // Se a rolagem horizontal for maior que 0, ajuste a posição dos botões
+            if (scrollLeft > 0) {
+                // Substitua ".manual-navigation" pelo seletor real do seu contêiner de botões
+                document.querySelector(".manual-navigation").style.position = "relative";
+                document.querySelector(".manual-navigation").style.left = -scrollLeft + "px";
+            } else {
+                // Caso contrário, restaure a posição original
+                document.querySelector(".manual-navigation").style.position = "static";
+                document.querySelector(".manual-navigation").style.left = "auto";
+            }
+        }
+
+     
+    
+
